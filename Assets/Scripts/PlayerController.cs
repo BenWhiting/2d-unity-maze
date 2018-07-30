2dc6f3f78cec1059e25da0a500cf87664e97b314
Ben Whiting <BenWhiting@benwhiting04@gmail.com>
2018-07-29 18:33:53 -0700

    Updating code

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody2D playerRb;
+    private Vector2 moveVelocity;
+    bool candMove = true;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+            Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            moveVelocity = moveInput.normalized * speed;
+
+
+        if (candMove)
+        {
+            playerRb.MovePosition(playerRb.position + moveVelocity * Time.fixedDeltaTime);
+        }
+
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision");
+    }
+}
